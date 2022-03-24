9fa62de68b7ca2ade9a921c50686d653950bcb90
CrmzonComet <lcameronbrew@gmail.com>
2022-03-23 22:52:19 -0400

    Level select functioning

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -9,7 +9,8 @@ public class SceneLoader : MonoBehaviour
     public void playGame() 
     {
     // change to the game scene
-SceneManager.LoadScene ("LevelOne");
+        SceneManager.LoadScene ("LevelOne");
+        // Debug.Log("Launch lvl 1");
     }
 
     //quit button
@@ -18,4 +19,29 @@ SceneManager.LoadScene ("LevelOne");
         Application.Quit();
         Debug.Log("Quit!");
     }
+
+    // level select screen
+    // level one button
+    public void levelOneSelect()
+    {
+        // change to level one game scene
+        SceneManager.LoadScene("LevelOne");
+        // Debug.Log("Launch lvl 1");
+    }
+    // level two button
+    public void levelTwoSelect()
+    {
+        // change to level two game scene
+        SceneManager.LoadScene("LevelTwo");
+        // Debug.Log("Launch lvl 2");
+    }
+
+    // level three button
+    public void levelThreeSelect()
+    {
+        // change to level three game scene
+        SceneManager.LoadScene("LevelThree");
+        // Debug.Log("Launch lvl 3");
+    }
+    
 }
